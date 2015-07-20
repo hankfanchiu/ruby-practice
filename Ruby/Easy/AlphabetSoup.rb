2f019c25f4562a61f68767df7ef950c0bf159c63
Hank Fanchiu <t5um3@users.noreply.github.com>
2015-07-19 23:48:02 -0700

    Create AlphabetSoup.rb

---
+++ Ruby/Easy/AlphabetSoup.rb
@@ -0,0 +1,16 @@
+# Have the function AlphabetSoup(str) take the str string parameter being passed and
+# return the string with the letters in alphabetical order (ie. hello becomes ehllo).
+
+# Assume numbers and punctuation symbols will not be included in the string. 
+
+#Use the Parameter Testing feature in the box below to test your code with different arguments.
+
+def AlphabetSoup(str)
+
+  return str.split("").sort.join("")
+
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+AlphabetSoup(STDIN.gets)
