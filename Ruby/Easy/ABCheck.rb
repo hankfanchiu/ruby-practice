250c0a11f554790c7ec2dd226e899ea2a655a7ac
Hank Fanchiu <t5um3@users.noreply.github.com>
2015-07-19 23:54:01 -0700

    Create ABCheck.rb

---
+++ Ruby/Easy/ABCheck.rb
@@ -0,0 +1,16 @@
+# Have the function ABCheck(str) take the str parameter being passed and
+# return the string true if the characters a and b are separated by exactly 3 places
+# anywhere in the string at least once (ie. "lane borrowed" would result in true
+# because there is exactly three characters between a and b). Otherwise return the string false. 
+
+#Use the Parameter Testing feature in the box below to test your code with different arguments.
+
+def ABCheck(str)
+
+  str =~ (/[a]...[b]/) ? true : false
+
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+ABCheck(STDIN.gets)           
