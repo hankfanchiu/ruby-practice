0a178008c680cf717f0d2d068d877d5a64fc201f
Hank Fanchiu <t5um3@users.noreply.github.com>
2015-07-19 22:37:03 -0700

    Update LetterCapitalize.rb

--- Ruby/Easy/LetterCapitalize.rb
+++ Ruby/Easy/LetterCapitalize.rb
@@ -2,3 +2,13 @@
 # Words will be separated by only one space. 
 
 # Use the Parameter Testing feature in the box below to test your code with different arguments.
+
+def LetterCapitalize(str)
+
+  return str.split(" ").map(&:capitalize).join(" ")
+  
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+LetterCapitalize(STDIN.gets)   
