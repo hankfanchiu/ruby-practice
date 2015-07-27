15dc79bb0abc6c16c5da8d1454b06def81bc73ee
Hank Fanchiu <t5um3@users.noreply.github.com>
2015-07-27 15:29:30 -0700

    Create BinaryConverter.rb

---
+++ Ruby/Easy/BinaryConverter.rb
@@ -0,0 +1,14 @@
+# Have the function BinaryConverter(str) return the decimal form of the binary value.
+# For example: if 101 is passed return 5, or if 1000 is passed return 8. 
+
+# Use the Parameter Testing feature in the box below to test your code with different arguments.
+
+def BinaryConverter(str)
+
+  return str.to_i(2)
+
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+BinaryConverter(STDIN.gets)           
