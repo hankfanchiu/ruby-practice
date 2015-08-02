f79eb6af19d830544d03c9578c7bf798940bf662
Hank Fanchiu <t5um3@users.noreply.github.com>
2015-08-02 14:05:20 -0700

    Update PowersofTwo.rb

--- Ruby/Easy/PowersofTwo.rb
+++ Ruby/Easy/PowersofTwo.rb
@@ -3,25 +3,19 @@
 # For example if the input is 16 then your program should return the string true but if the input is 22
 # then the output should be the string false. 
 
-# Use the Parameter Testing feature in the box below to test your code with different arguments.
-
 def PowersofTwo(num)
 
   power = Math.log(num) / Math.log(2)
   power % 1 == 0
-
 end
- 
-# keep this function call here 
-# to see how to enter arguments in Ruby scroll down   
-PowersofTwo(STDIN.gets)           
+
 
 # Another method without using the existing Math module:
 def PowersofTwo(num)
   i = 0
   while 2 ** i <= num
     if 2 ** i == num
-        return true
+      return true
     end
     i += 1
   end
