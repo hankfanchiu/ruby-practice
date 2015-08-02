4099f6e8792e8a0c88ca0d3929cf55ea755d30cc
Hank Fanchiu <t5um3@users.noreply.github.com>
2015-08-02 13:54:13 -0700

    Update LetterChanges.rb

--- Ruby/Easy/LetterChanges.rb
+++ Ruby/Easy/LetterChanges.rb
@@ -2,17 +2,10 @@
 # Replace every letter in the string with the letter following it in the alphabet (ie. c becomes d, z becomes a).
 # Then capitalize every vowel in this new string (a, e, i, o, u) and finally return this modified string. 
 
-# Use the Parameter Testing feature in the box below to test your code with different arguments.
-
 def LetterChanges(str)
 
-  str.gsub!(/[a-zA-Z]/) { |l| l.next }
-  str.gsub!(/[aeiou]/) { |v| v.upcase }
+  str.gsub!(/[a-zA-Z]/) { |letter| letter.next }
+  str.gsub!(/[aeiou]/) { |vowel| vowel.upcase }
   
   return str
-  
 end
-   
-# keep this function call here 
-# to see how to enter arguments in Ruby scroll down   
-LetterChanges(STDIN.gets)  
