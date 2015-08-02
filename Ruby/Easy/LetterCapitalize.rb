fc65e8942185422f9a2393bf720a2dfeb6da9510
Hank Fanchiu <t5um3@users.noreply.github.com>
2015-08-02 13:52:20 -0700

    Update LetterCapitalize.rb

--- Ruby/Easy/LetterCapitalize.rb
+++ Ruby/Easy/LetterCapitalize.rb
@@ -1,14 +1,7 @@
-# Have the function LetterCapitalize(str) take the str parameter being passed and capitalize the first letter of each word.
-# Words will be separated by only one space. 
-
-# Use the Parameter Testing feature in the box below to test your code with different arguments.
+# Have the function LetterCapitalize(str) take the str parameter being passed and
+# capitalize the first letter of each word. Words will be separated by only one space. 
 
 def LetterCapitalize(str)
 
-  return str.split(" ").map(&:capitalize).join(" ")
-  
+  return str.split.map(&:capitalize).join(" ")
 end
-   
-# keep this function call here 
-# to see how to enter arguments in Ruby scroll down   
-LetterCapitalize(STDIN.gets)   
