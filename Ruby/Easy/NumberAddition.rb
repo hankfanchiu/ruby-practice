d97b08e174e406c311a77c1e5dcfbba321c647b5
Hank Fanchiu <t5um3@users.noreply.github.com>
2015-08-02 14:02:13 -0700

    Update NumberAddition.rb

--- Ruby/Easy/NumberAddition.rb
+++ Ruby/Easy/NumberAddition.rb
@@ -1,33 +1,32 @@
 # Have the function NumberSearch(str) take the str parameter,
-# search for all the numbers in the string, add them together, then return that final number. 
+# search for all the numbers in the string, add them together, then return that final number.
+
 # For example: if str is "88Hello 3World!" the output should be 91.
 # You will have to differentiate between single digit numbers and
 # multiple digit numbers like in the example above.
 # So "55Hello" and "5Hello 5" should return two different answers.
-# Each string will contain at least one letter or symbol. 
 
-# Use the Parameter Testing feature in the box below to test your code with different arguments.
+# Each string will contain at least one letter or symbol. 
 
 # My solution:
 def NumberAddition(str)
 
   arr = str.scan(/\d+/).map(&:to_i)
-  arr == [] ? 0 : arr.reduce(:+)
   
+  if arr == []
+    return 0
+  else
+    arr.reduce(:+)
+  end
 end
 
 # Adapted from another person's solution that I like:
 def NumberAdditionII(str)
 
   sum = 0
-  numbers = str.scan(/\d+/).map(&:to_i)
   
+  numbers = str.scan(/\d+/).map(&:to_i)
   numbers.each { |n| sum += n }
   
-  sum
-
+  return sum
 end
-
-# keep this function call here 
-# to see how to enter arguments in Ruby scroll down   
-NumberAddition(STDIN.gets)         
