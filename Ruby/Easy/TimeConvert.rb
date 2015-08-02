b5efec358734fc2b497a296c4a5078aec8b6e775
Hank Fanchiu <t5um3@users.noreply.github.com>
2015-08-02 14:14:54 -0700

    Update TimeConvert.rb

--- Ruby/Easy/TimeConvert.rb
+++ Ruby/Easy/TimeConvert.rb
@@ -1,16 +1,9 @@
 # Have the function TimeConvert(num) take the num parameter being passed and return the number of hours and minutes
 # the parameter converts to (ie. if num = 63 then the output should be 1:3).
 
-#Separate the number of hours and minutes with a colon. 
-
-#Use the Parameter Testing feature in the box below to test your code with different arguments.
+# Separate the number of hours and minutes with a colon. 
 
 def TimeConvert(num)
 
   return "#{num / 60}:#{num % 60}"
-  
 end
-   
-# keep this function call here 
-# to see how to enter arguments in Ruby scroll down   
-TimeConvert(STDIN.gets)   
