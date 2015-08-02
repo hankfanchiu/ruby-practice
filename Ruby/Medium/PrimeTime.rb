63ca86b11bfb1bbfcec93dfdab063846fd15c94e
Hank Fanchiu <t5um3@users.noreply.github.com>
2015-08-02 14:17:52 -0700

    Update PrimeTime.rb

--- Ruby/Medium/PrimeTime.rb
+++ Ruby/Medium/PrimeTime.rb
@@ -1,11 +1,14 @@
 # Have the function PrimeTime(num) take the num parameter being passed and
 # return the string true if the parameter is a prime number, otherwise return the string false.
-# The range will be between 1 and 2^16. 
 
-# Use the Parameter Testing feature in the box below to test your code with different arguments.
+# The range will be between 1 and 2^16. 
 
 def PrimeTime(num)
 
+  if num < 2
+    return false
+  end
+  
   i = 2
   while i ** 2 <= num
     if num % i == 0
@@ -16,7 +19,3 @@ def PrimeTime(num)
   
   return true
 end
-
-# keep this function call here 
-# to see how to enter arguments in Ruby scroll down   
-PrimeTime(STDIN.gets)   
