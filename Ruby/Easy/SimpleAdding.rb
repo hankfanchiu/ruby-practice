a3832e0a031a68e2cdced6882cee0d44fb82824e
Hank Fanchiu <t5um3@users.noreply.github.com>
2015-08-02 14:08:14 -0700

    Update SimpleAdding.rb

--- Ruby/Easy/SimpleAdding.rb
+++ Ruby/Easy/SimpleAdding.rb
@@ -1,14 +1,10 @@
 # Have the function SimpleAdding(num) add up all the numbers from 1 to num.
-# For the test cases, the parameter num will be any number from 1 to 1000. 
 
-# Use the Parameter Testing feature in the box below to test your code with different arguments.
+# For the test cases, the parameter num will be any number from 1 to 1000. 
 
 def SimpleAdding(num)
 
-  return (1..num).to_a.inject { |sum, x| sum + x }
+  arr = (1..num).to_a
   
+  return arr.inject(:+)
 end
-   
-# keep this function call here 
-# to see how to enter arguments in Ruby scroll down   
-SimpleAdding(STDIN.gets)   
