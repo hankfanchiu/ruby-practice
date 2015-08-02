5ff11150aedfcdc0c28a9bed6c5e389c5f3dbc9b
Hank Fanchiu <t5um3@users.noreply.github.com>
2015-08-02 14:15:15 -0700

    Update VowelCount.rb

--- Ruby/Easy/VowelCount.rb
+++ Ruby/Easy/VowelCount.rb
@@ -2,14 +2,7 @@
 # return the number of vowels the string contains (ie. "All cows eat grass" would return 5).
 # Do not count y as a vowel for this challenge. 
 
-# Use the Parameter Testing feature in the box below to test your code with different arguments.
-
 def VowelCount(str)
 
-  return str.scan(/[aeiou]/).size
-
+  return str.scan(/[aeiou]/).count
 end
-   
-# keep this function call here 
-# to see how to enter arguments in Ruby scroll down   
-VowelCount(STDIN.gets)  
