cc0c5daebe7aea825fe9755cb54cdfb4adb985cd
Hank Fanchiu <t5um3@users.noreply.github.com>
2015-08-02 13:51:38 -0700

    Update FirstReverse.rb

--- Ruby/Easy/FirstReverse.rb
+++ Ruby/Easy/FirstReverse.rb
@@ -1,13 +1,6 @@
 # Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. 
-# Use the Parameter Testing feature in the box below to test your code with different arguments.
 
 def FirstReverse(str)
 
-  str = str.reverse
-  return str 
-  
+  return str.reverse
 end
- 
-# keep this function call here 
-# to see how to enter arguments in Ruby scroll down   
-FirstReverse(STDIN.gets)
