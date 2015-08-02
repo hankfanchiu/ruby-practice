ccf4358a8ae6c07998cbe238a5b1a8e069b8e768
Hank Fanchiu <t5um3@users.noreply.github.com>
2015-08-02 13:54:57 -0700

    Update LetterCountI.rb

--- Ruby/Easy/LetterCountI.rb
+++ Ruby/Easy/LetterCountI.rb
@@ -1,9 +1,9 @@
 # Have the function LetterCountI(str) take the str parameter being passed and return the first word
 # with the greatest number of repeated letters. For example: "Today, is the greatest day ever!" should
 # return greatest because it has 2 e's (and 2 t's) and it comes before ever which also has 2 e's.
+
 # If there are no words with repeating letters return -1. Words will be separated by spaces. 
 
-# Use the Parameter Testing feature in the box below to test your code with different arguments.
 
 # This first method returns the word with the highest number of instances of repeating letters:
 # For example, if "aaa bbcc" was the input, the return would be "bbcc". This is because there are
@@ -27,9 +27,6 @@ def LetterCountI(str)
   end
 end
 
-# keep this function call here 
-# to see how to enter arguments in Ruby scroll down   
-LetterCountI(STDIN.gets)  
 
 # This second method correctly identifies the word with highest number of repeating letters:
 # For example, if "aaa bbcc" was the input, the return would be "aaa".
@@ -65,5 +62,5 @@ def LetterCountIII(str)
       end
     end
   end
-  answer
+  return answer
 end
