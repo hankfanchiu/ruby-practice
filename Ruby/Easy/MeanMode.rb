e212a554fed79873dca87aba2f54b1581abb91d4
Hank Fanchiu <t5um3@users.noreply.github.com>
2015-08-02 13:57:25 -0700

    Update MeanMode.rb

--- Ruby/Easy/MeanMode.rb
+++ Ruby/Easy/MeanMode.rb
@@ -1,9 +1,9 @@
 # Have the function MeanMode(arr) take the array of numbers stored in arr
 # and return 1 # if the mode equals the mean, 0 if they don't equal each other
 # (ie. [5, 3, 3, 3, 1] should return 1 because the mode (3) equals the mean (3)).
+
 # The array will not be empty, will only contain positive integers, and will not contain more than one mode. 
 
-# Use the Parameter Testing feature in the box below to test your code with different arguments.
 
 def MeanMode(arr)
 
@@ -15,6 +15,9 @@ def MeanMode(arr)
   # Determine the mean
   mean =  arr.reduce(:+) / arr.length
   
-  mode == mean ? 1 : 0
-
+  if mode == mean
+    return 1
+  else
+    return 0
+  end
 end
