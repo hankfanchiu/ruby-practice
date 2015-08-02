f447e5665f0264eac3d5934179787749f48d2305
Hank Fanchiu <t5um3@users.noreply.github.com>
2015-08-02 13:59:33 -0700

    Update MultiplicativePersistence.rb

--- Ruby/Easy/MultiplicativePersistence.rb
+++ Ruby/Easy/MultiplicativePersistence.rb
@@ -5,21 +5,16 @@
 # For example: if num is 39 then your program should return 3 because 3 * 9 = 27
 # then 2 * 7 = 14 and finally 1 * 4 = 4 and you stop at 4. 
 
-# Use the Parameter Testing feature in the box below to test your code with different arguments.
-
 def MultiplicativePersistence(num)
 
+  reduced = num
   counter = 0
-  until num.to_s.size == 1
+  
+  until reduced.to_s.size == 1
     # Replace the parameter with product of its digits
-    num = num.to_s.chars.map(&:to_i).reduce(:*)
-    
+    reduced = num.to_s.chars.map(&:to_i).reduce(:*)
     counter += 1
   end
   
   return counter
 end
-
-# keep this function call here 
-# to see how to enter arguments in Ruby scroll down   
-MultiplicativePersistence(STDIN.gets)
