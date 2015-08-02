9e09809e838ac401e6a9f60b41893af120dd1ddb
Hank Fanchiu <t5um3@users.noreply.github.com>
2015-08-02 13:29:55 -0700

    Update AlphabetSoup.rb

--- Ruby/Easy/AlphabetSoup.rb
+++ Ruby/Easy/AlphabetSoup.rb
@@ -3,14 +3,8 @@
 
 # Assume numbers and punctuation symbols will not be included in the string. 
 
-#Use the Parameter Testing feature in the box below to test your code with different arguments.
-
 def AlphabetSoup(str)
 
   return str.chars.sort.join
 
 end
-   
-# keep this function call here 
-# to see how to enter arguments in Ruby scroll down   
-AlphabetSoup(STDIN.gets)
