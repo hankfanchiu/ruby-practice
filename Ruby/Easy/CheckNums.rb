fe34a51a1ef0be4b78ff23faa099b0a3db2feb15
Hank Fanchiu <t5um3@users.noreply.github.com>
2015-08-02 13:34:46 -0700

    Update CheckNums.rb

--- Ruby/Easy/CheckNums.rb
+++ Ruby/Easy/CheckNums.rb
@@ -1,8 +1,7 @@
 # Have the function CheckNums(num1,num2) take both parameters being passed and return the string true
 # if num2 is greater than num1, otherwise return the string false.
-# If the parameter values are equal to each other then return the string -1. 
 
-#Use the Parameter Testing feature in the box below to test your code with different arguments.
+# If the parameter values are equal to each other then return the string -1. 
 
 def CheckNums(num1,num2)
 
@@ -15,7 +14,3 @@ def CheckNums(num1,num2)
   end 
   
 end
-   
-# keep this function call here 
-# to see how to enter arguments in Ruby scroll down   
-CheckNums(STDIN.gets)   
