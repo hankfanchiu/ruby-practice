dad1e4cd4f30d5cbee4e7971780fab35707cc35f
Hank Fanchiu <t5um3@users.noreply.github.com>
2015-08-11 23:19:36 -0700

    Create DashInsertII.rb

---
+++ Ruby/Medium/DashInsertII.rb
@@ -0,0 +1,28 @@
+# Have the function DashInsertII(str) insert dashes ('-') between each two odd numbers
+# and insert asterisks ('*') between each two even numbers in str.
+
+# For example: if str is 4546793 the output should be 454*67-9-3.
+# Don't count zero as an odd or even number.
+
+def DashInsertII(num)
+
+  arr = num.to_s.split(//)
+  answer = Array.new
+  
+  arr.each_with_index do |n, i|
+    answer << n
+    
+    if (n.to_i != 0) && (arr[i + 1].to_i != 0)
+      # Between two odd numbers:
+      if (n.to_i % 2 != 0) && (arr[i + 1].to_i % 2 != 0)
+        answer << '-'
+        
+      # Between two even numbers:
+      elsif (n.to_i % 2 == 0) && (arr[i + 1].to_i % 2 == 0)
+        answer << '*'
+      end
+    end
+  end
+  
+  return answer.join       
+end
