79040237446ca70357802b9909cbce76a7fb6409
Hank Fanchiu <fanchiu.hank@gmail.com>
2015-08-24 17:37:31 -0700

    Update OverlappingRectangles.rb to separate the function of determining the overlapping x- and y-ranges

--- Ruby/Medium/OverlappingRectangles.rb
+++ Ruby/Medium/OverlappingRectangles.rb
@@ -19,58 +19,40 @@
 
 def OverlappingRectangles(strArr)
 
+  # Parse the input string into arrays of coordinates for two rectangles:
   n = strArr.scan(/-?\d+/).map(&:to_i)
   r1 = [[n[0], n[1]], [n[2], n[3]], [n[4], n[5]], [n[6], n[7]]]
   r2 = [[n[8], n[9]], [n[10], n[11]], [n[12], n[13]], [n[14], n[15]]]
 
-  # Determine the rectangles' relative x-positions.
-  r_left = r1
-  r_right = r2
-
-  if r2.min[0] < r1.min[0]
-    r_left = r2
-    r_right = r1
-  end
-
-  # No overlapping x:
-  return 0 if r_left.max[0] < r_right.min[0]
-
-  # Overlapping x:
-  smaller_x_max = r_left.max[0]
-  smaller_x_max = r_right.max[0] if r_right.max[0] < r_left.max[0]
-
-  overlapping_x_range = smaller_x_max - r_right.min[0]
-
-  # Determine the rectangles' relative y-positions
-  r_bottom = r1
-  r_top = r2
-
-  if r2.min[1] < r1.min[1]
-    r_bottom = r2
-    r_top = r1
-  end
-
-  # No overlapping y:
-  return 0 if r_bottom.max[1] < r_top.min[1]
-
-  # Overlapping y:
-  smaller_y_max = r_bottom.max[1]
-  smaller_y_max = r_top.max[1] if r_top.max[1] < r_bottom.max[1]
-
-  overlapping_y_range = smaller_y_max - r_top.min[1]
-
   # Area of overlap:
-  area = overlapping_x_range.abs * overlapping_y_range.abs
+  x_range = overlap(r1, r2, 0)
+  y_range = overlap(r1, r2, 1)
+  area = (x_range * y_range).abs
 
   # Area of rectangle 1:
   r1_x_range = r1.max[0] - r1.min[0]
   r1_y_range = r1.max[1] - r1.min[1]
-  r1_area = r1_x_range.abs * r1_y_range.abs
+  r1_area = (r1_x_range * r1_y_range).abs
 
-  return r1_area / area
+  area == 0? 0 : r1_area / area
 end
 
 
-puts 'OverlappingRectangles("(0,0),(5,0),(0,2),(5,2),(2,1),(5,1),(2,-1),(5,-1)"):'
-puts OverlappingRectangles("(0,0),(5,0),(0,2),(5,2),(2,1),(5,1),(2,-1),(5,-1)")
-puts
+# Separate method to determine the overlapping ranges between two rectangles
+def overlap(r1, r2, i)
+
+  # Determine the rectangles' relative x- or y-positions.
+  left_or_bot = r1
+  right_or_top = r2
+
+  if r2.min[i] < r1.min[i]
+    left_or_bot = r2
+    right_or_top = r1
+  end
+
+  # Return 0 if there is no overlap, or else return the overlapping range.
+  return 0 if left_or_bot.max[i] < right_or_top.min[i]
+  return [left_or_bot.max[i], right_or_top.max[i]].min - right_or_top.min[i]
+end
+
+puts OverlappingRectangles("(0,0),(0,-2),(3,0),(3,-2),(2,-1),(3,-1),(2,3),(3,3)")
