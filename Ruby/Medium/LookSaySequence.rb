26463319c315a52329edbbd8c4f13d1f47495521
Hank Fanchiu <fanchiu.hank@gmail.com>
2015-08-24 19:48:05 -0700

    Create LookSaySequence.rb to return the next number in a look-say sequence

---
+++ Ruby/Medium/LookSaySequence.rb
@@ -0,0 +1,34 @@
+# Using the Ruby language, have the function LookSaySequence(num)
+# take the num parameter being passed and return the next number
+# in the sequence according to the following rule: to generate the next number
+# in a sequence read off the digits of the given number,
+# counting the number of digits in groups of the same digit.
+#
+# For example, the sequence beginning with 1 would be: 1, 11, 21, 1211, ...
+# The 11 comes from there being "one 1" before it and
+# the 21 comes from there being "two 1's" before it.
+#
+# So your program should return the next number in the sequence given num.
+
+def LookSaySequence(num)
+
+  digits = num.to_s.split('')
+  answer = Array.new
+  counter = 1
+
+  digits.each_with_index do |n, i|
+    if n == digits[i + 1]
+      counter += 1
+    else
+      answer << counter.to_s
+      answer << n
+      counter = 1
+    end
+  end
+
+  return answer.join.to_i
+end
+
+puts LookSaySequence(1211)
+# => 111221
+
