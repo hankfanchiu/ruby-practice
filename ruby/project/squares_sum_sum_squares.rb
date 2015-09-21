88026096b37b320ce4d8293f328a2a49435b193a
Hank Fanchiu <hank@fanchiu.com>
2015-09-21 12:06:52 -0700

    Update with a more efficient method

--- ruby/project/squares_sum_sum_squares.rb
+++ ruby/project/squares_sum_sum_squares.rb
@@ -17,3 +17,17 @@ end
 
 puts squares_sum_sum_squares(10) == 2640
 puts squares_sum_sum_squares(100)
+
+
+# More efficient method (constant time O(1)):
+def squares_sum_2(num)
+
+  sum = num * (num + 1) / 2
+  squared = (num * (num + 1) * (2 * num + 1)) / 6
+
+  (sum * sum) - squared
+end
+
+
+puts squares_sum_2(10) == 2640
+puts squares_sum_2(100)
