dcf4c9e7883eb644fe995179bd72ebe3c68cbc5d
Hank Fanchiu <hank@fanchiu.com>
2015-09-20 20:11:41 -0700

    Add new method and fix formatting

---
+++ ruby/project/max_product_adjacent_integers.rb
@@ -0,0 +1,74 @@
+# Given a large integer (num) and a number of digits (digits)
+# find the greatest product of the number of adjacent digits.
+#
+# Assume that none of the digits in the input number is a zero.
+
+def max_product(num, digits)
+
+  integers = num.to_s.split('').map(&:to_i)
+  product = integers[0, digits].inject(:*)
+  max = product
+
+  i = 0
+  while i < integers.count - digits
+    product /= integers[i]
+    product *= integers[i + digits]
+
+    max = product if product > max
+
+    i += 1
+  end
+
+  max
+end
+
+
+# The four adjacent digits in the 1000-digit number
+# that have the greatest product are 9 × 9 × 8 × 9 = 5832.
+
+test = '73167176531330624919225119674426574742355349194934' +
+  '96983520312774506326239578318016984801869478851843' +
+  '85861560789112949495459501737958331952853208805511' +
+  '12540698747158523863050715693290963295227443043557' +
+  '66896648950445244523161731856403098711121722383113' +
+  '62229893423380308135336276614282806444486645238749' +
+  '30358907296290491560440772390713810515859307960866' +
+  '70172427121883998797908792274921901699720888093776' +
+  '65727333001053367881220235421809751254540594752243' +
+  '52584907711670556013604839586446706324415722155397' +
+  '53697817977846174064955149290862569321978468622482' +
+  '83972241375657056057490261407972968652414535100474' +
+  '82166370484403199890008895243450658541227588666881' +
+  '16427171479924442928230863465674813919123162824586' +
+  '17866458359124566529476545682848912883142607690042' +
+  '24219022671055626321111109370544217506941658960408' +
+  '07198403850962455444362981230987879927244284909188' +
+  '84580156166097919133875499200524063689912560717606' +
+  '05886116467109405077541002256983155200055935729725' +
+  '71636269561882670428252483600823257530420752963450'
+
+test = test.to_i
+
+# Find the thirteen adjacent digits in the 1000-digit number
+# that have the greatest product.
+#
+# What is the value of this product?
+
+def adjacent_product(num, digits)
+
+  integers = num.to_s.split('').map(&:to_i)
+  max = 0
+
+  i = 0
+  while i < integers.count - digits
+    product = integers[i, digits].inject(:*)
+    max = product if product > max
+
+    i += 1
+  end
+
+  max
+end
+
+puts adjacent_product(test, 4) == 5832
+puts adjacent_product(test, 13)
