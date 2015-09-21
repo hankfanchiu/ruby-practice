5115fc19c7d53dde0207b818a7e090a1704855e2
Hank Fanchiu <hank@fanchiu.com>
2015-09-20 18:58:11 -0700

    Move and update squares and sums

--- ruby/project/squares_sum_sum_squares.rb
+++ ruby/project/squares_sum_sum_squares.rb
@@ -2,12 +2,18 @@
 # the sum of the squares of the integers from 1 to number,
 # and the square of the sum of the integers from 1 to number.
 
-def squares_sum_sum_squares(number)
+def squares_sum_sum_squares(num)
 
-  sum_of_squares = (1..number).map { |num| num ** 2 }.inject(:+)
-  square_of_sum = ((1..number).inject(:+)) ** 2
+  sum = 0
+  squares_sum = 0
 
-  square_of_sum - sum_of_squares
+  (1..num).each do |number|
+    sum += number
+    squares_sum += number ** 2
+  end
+
+  (sum ** 2) - squares_sum
 end
 
 puts squares_sum_sum_squares(10) == 2640
+puts squares_sum_sum_squares(100)
