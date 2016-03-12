61c9421868e4be6d3d5a69c54d7e578c2172c8b6
Hank Fanchiu <fanchiu.hank@gmail.com>
2016-03-11 17:49:39 -0800

    Refactor stock picker to use better variables

--- misc/stock_picker.rb
+++ misc/stock_picker.rb
@@ -1,36 +1,40 @@
 def stock_picker(stocks)
-  minimum = stocks.first
-  minimum_index = 0
-  picked_stock = [0, nil]
-  picked_stock_difference = 0
+  min_price_index = 0
+  picked_indexes = { buy: 0, sell: nil }
+  picked_difference = 0
 
-  1.upto(stocks.length - 1) do |index|
-    current = stocks[index]
+  1.upto(stocks.length - 1) do |current_index|
+    current_price = stocks[current_index]
 
-    if current < minimum
-      minimum = current
-      minimum_index = index
+    if current_price < stocks[min_price_index]
+      min_price_index = current_index
       next
     end
 
-    original_buy_difference = current - stocks[picked_stock.first]
+    original_buy_index = picked_indexes[:buy]
+    original_buy = stocks[original_buy_index]
+    original_buy_difference = current_price - original_buy
 
-    if original_buy_difference > picked_stock_difference
-      picked_stock[1] = index
-      picked_stock_difference = original_buy_difference
+    if original_buy_difference >= picked_difference
+      picked_indexes[:sell] = current_index
+      picked_difference = original_buy_difference
     end
 
-    next if picked_stock.first == minimum_index
+    next if picked_indexes[:buy] == min_price_index
 
-    new_buy_difference = current - minimum
+    new_buy = stocks[min_price_index]
+    new_buy_difference = current_price - new_buy
 
-    if new_buy_difference > picked_stock_difference
-      picked_stock = [minimum_index, index]
-      picked_stock_difference = new_buy_difference
+    if new_buy_difference >= picked_difference
+      picked_indexes[:buy] = min_price_index
+      picked_indexes[:sell] = current_index
+      picked_difference = new_buy_difference
     end
   end
 
-  picked_stock
+  return nil unless picked_indexes[:sell]
+
+  [picked_indexes[:buy], picked_indexes[:sell]]
 end
 
-p stock_picker([6, 7, 1, 10])
+p stock_picker([4, 3, 2, 1])
